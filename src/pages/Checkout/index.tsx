847f73a84ee6fffb9a89348707ced95f7d54d07b
Gabriela França <gabifmouraroc@outlook.com>
2024-01-22 11:52:36 -0300

    estilização da página checkout, área de endereço e área de pagamento

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,60 @@
+import { CurrencyDollar, MapPinLine } from 'phosphor-react'
+import {
+  Address,
+  AddressAndPayment,
+  CheckoutArea,
+  CompleteOrderArea,
+  InputArea,
+  MapIcon,
+  MoneyIcon,
+  Payment,
+  Title,
+} from './styles'
+
 export function Checkout() {
-  return <h1>Checkout</h1>
+  return (
+    <CheckoutArea>
+      <CompleteOrderArea>
+        <h1>Complete seu pedido</h1>
+        <AddressAndPayment>
+          <Title>
+            <MapIcon>
+              <MapPinLine size={24} />
+            </MapIcon>
+            <div>
+              <h2>Endereço de Entrega</h2>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </div>
+          </Title>
+          <InputArea>
+            <input type="number" className="cep" placeholder="CEP" />
+            <input type="text" className="rua" placeholder="Rua" />
+            <input type="number" className="num" placeholder="Número" />
+            <input
+              type="text"
+              className="complemento"
+              placeholder="Complemento"
+            />
+            <input type="text" className="bairro" placeholder="Bairro" />
+            <input type="text" className="cidade" placeholder="Cidade" />
+            <input type="text" className="uf" placeholder="UF" />
+          </InputArea>
+        </AddressAndPayment>
+
+        <AddressAndPayment>
+          <Title>
+            <MoneyIcon>
+              <CurrencyDollar size={24}/>
+            </MoneyIcon>
+            <div>
+              <h2>Pagamento</h2>
+              <span>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </span>
+            </div>
+          </Title>
+        </AddressAndPayment>
+      </CompleteOrderArea>
+    </CheckoutArea>
+  )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,91 @@
+import styled from 'styled-components'
+
+export const CheckoutArea = styled.div`
+  margin-top: 4rem;
+`
+
+export const CompleteOrderArea = styled.div`
+  h1 {
+    font-size: 1.125rem;
+  }
+`
+
+export const AddressAndPayment = styled.div`
+  margin-top: 1rem;
+  background-color: ${(props) => props.theme['base-card']};
+  padding: 2.5rem;
+  border-radius: 6px;
+`
+
+export const Title = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  margin-bottom: 2rem;
+
+  h2 {
+    font-weight: normal;
+    font-size: 1rem;
+    margin-bottom: 0.3rem;
+  }
+
+  span {
+    font-size: 0.875rem;
+  }
+`
+
+export const MapIcon = styled.div`
+  color: ${(props) => props.theme['yellow-dark']};
+`
+export const InputArea = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-areas:
+    'cep . . .'
+    'rua rua rua rua'
+    'num complemento complemento complemento'
+    'bairro cidade cidade uf';
+
+  input {
+    padding: 0.75rem;
+    border-radius: 4px;
+    background-color: ${(props) => props.theme['base-input']};
+    border: 1px solid ${(props) => props.theme['base-button']};
+    font-size: 0.875rem;
+
+    &::placeholder {
+      color: ${(props) => props.theme['base-label']};
+    }
+  }
+
+  .cep {
+    grid-area: cep;
+  }
+
+  .rua {
+    grid-area: rua;
+  }
+
+  .num {
+    grid-area: num;
+  }
+
+  .complemento {
+    grid-area: complemento;
+  }
+
+  .bairro {
+    grid-area: bairro;
+  }
+
+  .cidade {
+    grid-area: cidade;
+  }
+
+  .uf {
+    grid-area: uf;
+  }
+`
+
+export const MoneyIcon = styled.div`
+  color: ${(props) => props.theme.purple};
+`
