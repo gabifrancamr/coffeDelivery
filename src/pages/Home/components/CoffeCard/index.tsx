0a8908f182dbf2de6e11b568daee7594e40d176b
Gabriela França <gabifmouraroc@outlook.com>
2024-01-29 10:57:02 -0300

    manipulação do valor da quatidade de café e adição do café ao carrinho

--- src/pages/Home/components/CoffeCard/index.tsx
+++ src/pages/Home/components/CoffeCard/index.tsx
@@ -10,6 +10,8 @@ import {
 } from './styles'
 import { QuantityInput } from '../../../../components/QuantityInput'
 import { FormatMoney } from '../../../../utils/formatMoney'
+import { useCart } from '../../../../hooks/useCart'
+import { useState } from 'react'
 
 export interface Coffee {
   id: number
@@ -25,6 +27,27 @@ interface CoffeeProps {
 }
 
 export function CoffeeCard({ coffee }: CoffeeProps) {
+  const [quantity, setQuantity] = useState(1)
+
+  function handleIncreased() {
+    setQuantity((prevQuantity) => prevQuantity + 1)
+  }
+
+  function handleDecreased() {
+    setQuantity((prevQuantity) => prevQuantity - 1)
+  }
+
+  const { addCoffeeToCard } = useCart()
+
+  function handleAddCoffeeToCard() {
+    const coffeeToAdd = {
+      ...coffee,
+      quantity,
+    }
+
+    addCoffeeToCard(coffeeToAdd)
+  }
+
   const formattedPrice = FormatMoney(coffee.price)
   return (
     <CoffeeCardContainer>
@@ -47,8 +70,12 @@ export function CoffeeCard({ coffee }: CoffeeProps) {
         </div>
 
         <AddCartWrapper>
-          <QuantityInput />
-          <button>
+          <QuantityInput
+            onIncrease={handleIncreased}
+            onDecrease={handleDecreased}
+            quantity={quantity}
+          />
+          <button onClick={handleAddCoffeeToCard}>
             <ShoppingCart weight="fill" size={22} />
           </button>
         </AddCartWrapper>
