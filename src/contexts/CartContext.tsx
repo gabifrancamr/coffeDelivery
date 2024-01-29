c7a5fccb762291e71dcf2c9e5d26cdda49948596
Gabriela França <gabifmouraroc@outlook.com>
2024-01-29 10:25:13 -0300

    adicionando no contexto a função de adicionar café ao carrinho

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useState } from 'react'
 import { Coffee } from '../pages/Home/components/CoffeCard'
+import { produce } from 'immer'
 
 export interface CartItem extends Coffee {
   quantity: number
@@ -7,6 +8,7 @@ export interface CartItem extends Coffee {
 
 interface CartContextType {
   cartItems: CartItem[]
+  addCoffeeToCard: (coffee: CartItem) => void
 }
 
 export const CartContext = createContext({} as CartContextType)
@@ -18,8 +20,24 @@ interface CartContextProviderProps {
 export function CartContextProvider({ children }: CartContextProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([])
 
+  function addCoffeeToCard(coffee: CartItem) {
+    const coffeeAlreadyExistsInCard = cartItems.findIndex(
+      (cardItem) => cardItem.id === coffee.id,
+    )
+
+    const newCart = produce(cartItems, (draft) => {
+      if (coffeeAlreadyExistsInCard < 0) {
+        draft.push(coffee)
+      } else {
+        draft[coffeeAlreadyExistsInCard].quantity += coffee.quantity
+      }
+    })
+
+    setCartItems(newCart)
+  }
+
   return (
-    <CartContext.Provider value={{ cartItems }}>
+    <CartContext.Provider value={{ cartItems, addCoffeeToCard }}>
       {children}
     </CartContext.Provider>
   )
