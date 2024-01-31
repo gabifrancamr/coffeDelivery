8a34fe616b2fe2de6cbafd5316c9f16aa46b7e10
Gabriela França <gabifmouraroc@outlook.com>
2024-01-31 12:16:01 -0300

    soma de todos os itens do carrinho com frete

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -9,6 +9,7 @@ export interface CartItem extends Coffee {
 interface CartContextTypes {
   cartItems: CartItem[]
   cartQuantity: number
+  cartTotalPrice: number
   addCoffeeToCart: (coffee: CartItem) => void
   changeQuantityCoffee: (
     coffeeId: number,
@@ -27,6 +28,10 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([])
 
   const cartQuantity = cartItems.length
+
+  const cartTotalPrice = cartItems.reduce((valor, item) => {
+    return valor + item.price * item.quantity
+  }, 0)
 
   function addCoffeeToCart(coffee: CartItem) {
     const thereIsThisCoffeeInCart = cartItems.findIndex(
@@ -84,6 +89,7 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
         addCoffeeToCart,
         changeQuantityCoffee,
         removeCoffee,
+        cartTotalPrice,
       }}
     >
       {children}
--- src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
+++ src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
@@ -1,25 +1,36 @@
 import { Button } from '../../../../components/Button'
 import { RegularText } from '../../../../components/Typography'
+import { useCart } from '../../../../hooks/UseCart'
+import { FormatMoney } from '../../../../utils/formatMoney'
 
 import { ConfirmationSectionContainer } from './styles'
 
+const DELIVERY_PRICE = 3.5
+
 export function ConfirmationSection() {
+  const { cartTotalPrice } = useCart()
+  const priceWithDelivery = cartTotalPrice + DELIVERY_PRICE
+
+  const formattedDelivery = FormatMoney(DELIVERY_PRICE)
+  const formattedCartTotalPrice = FormatMoney(cartTotalPrice)
+  const formattedPriceWithDelivery = FormatMoney(priceWithDelivery)
+
   return (
     <ConfirmationSectionContainer>
       <div>
         <RegularText size="s">Total de itens</RegularText>
-        <RegularText>R$ 9,90 </RegularText>
+        <RegularText>R$ {formattedCartTotalPrice} </RegularText>
       </div>
       <div>
         <RegularText size="s">Entrega</RegularText>
-        <RegularText>R$ 3,50</RegularText>
+        <RegularText>R$ {formattedDelivery}</RegularText>
       </div>
       <div>
         <RegularText weight="700" color="subtitle" size="l">
           Total
         </RegularText>
         <RegularText weight="700" color="subtitle" size="l">
-          R$ 29,90
+          R$ {formattedPriceWithDelivery}
         </RegularText>
       </div>
       <Button text="confirmar pedido" />
